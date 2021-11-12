8f9ec7c724bed89057ace63207bc9ecff62d5ce3
Abhishek Mishra <abhishek@luezoid.com>
2021-11-12 19:56:09 +0530

    BaseRequest: keeping existing class and extending it from the new PSR complied class to maintain backward compatibility

---
+++ src/Requests/BaseRequest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luezoid
+ * Date: 12/18/17
+ * Time: 2:23 PM
+ */
+
+namespace Luezoid\Laravelcore\Requests;
+
+
+abstract class BaseRequest extends \Luezoid\Laravelcore\Http\Requests\BaseRequest
+{
+}
